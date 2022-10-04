48ba519cdb15c7568f8d5db0fa2d5d5f550c44f5
PieroMastro <pier.andres@gmail.com>
2022-10-04 14:27:00 +0000

    Fixed extensions issue

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import reactDom from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+
 	const [color, setColor] = useState("")
+
 	return (
 		<div className="trafficlight">
-			<div className={color === "red" ? "red on" : "red"} onClick={() => setColor("red")}></div>
-			<div className={color === "yellow" ? "yellow on" : "yellow"} onClick={() => setColor("yellow")}></div>
-			<div className={color === "green" ? "green on" : "green"} onClick={() => setColor("green")}></div>
+			<div
+				className={color === "red" ? "red on" : "red"}
+				onClick={() => setColor("red")}>
+			</div>
+			<div
+				className={color === "yellow" ? "yellow on" : "yellow"}
+				onClick={() => setColor("yellow")}>
+			</div>
+			<div
+				className={color === "green" ? "green on" : "green"}
+				onClick={() => setColor("green")}>
+			</div>
 		</div>
 	);
 };
